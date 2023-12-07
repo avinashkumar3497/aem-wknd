01c2da10f3511385865803c0d0f1b5f21e5f29ed
Avinash Kumar <avinashkumar3497@gmail.com>
2023-12-07 17:03:57 +0530

    Created a DemoService and it's implementation

---
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/services/impl/DemoServiceImpl.java
@@ -0,0 +1,30 @@
+package com.adobe.aem.guides.wknd.core.services.impl;
+
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.adobe.aem.guides.wknd.core.services.DemoService;
+
+@Component(service = DemoService.class, immediate = true)
+public class DemoServiceImpl implements DemoService {
+
+
+    private static final Logger LOG = LoggerFactory.getLogger(DemoServiceImpl.class);
+    
+    @Activate
+    public void activate(ComponentContext componentContext){
+        LOG.info("==============INSIDE ACTIVATE");
+        LOG.info("\n {} = {} ",componentContext.getBundleContext().getBundle().getBundleId(),componentContext.getBundleContext().getBundle().getSymbolicName());
+    }
+
+    @Deactivate
+    public void deactivate(){
+        LOG.info("==============INSIDE DeACTIVATE");
+    }
+
+
+}
