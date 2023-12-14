4421784140de92166f8601d2bca54cc3d13d536d
Avinash Kumar <avinashkumar3497@gmail.com>
2023-12-14 20:33:00 +0530

    implemented Factory Configuration

--- core/src/main/java/com/adobe/aem/guides/wknd/core/services/impl/DemoServiceImpl.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/services/impl/DemoServiceImpl.java
@@ -1,9 +1,15 @@
 package com.adobe.aem.guides.wknd.core.services.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.osgi.service.component.ComponentContext;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
 import org.osgi.service.component.propertytypes.ServiceRanking;
 import org.osgi.service.metatype.annotations.Designate;
 import org.slf4j.Logger;
@@ -13,19 +19,34 @@ import com.adobe.aem.guides.wknd.core.config.DemoConfig;
 import com.adobe.aem.guides.wknd.core.services.DemoService;
 
 @Component(service = DemoService.class, immediate = true)
-@Designate(ocd = com.adobe.aem.guides.wknd.core.config.DemoConfig.class)
+@Designate(ocd = com.adobe.aem.guides.wknd.core.config.DemoConfig.class, factory = true)
 @ServiceRanking(2)
 public class DemoServiceImpl implements DemoService {
 
 
     private static final Logger LOG = LoggerFactory.getLogger(DemoServiceImpl.class);
     private String someSetting;
+    private List<DemoConfig> configList;
     
     @Activate
-    public void activate(ComponentContext componentContext, DemoConfig config){
+    public void activate(ComponentContext componentContext){
         LOG.info("==============INSIDE ACTIVATE");
         LOG.info("\n {} = {} ",componentContext.getBundleContext().getBundle().getBundleId(),componentContext.getBundleContext().getBundle().getSymbolicName());
-        someSetting=config.someSetting();
+        someSetting="\n"+configList.get(0).someSetting()+"\n"+configList.get(1).someSetting();
+    }
+
+    @Reference(service = DemoConfig.class, cardinality = ReferenceCardinality.MULTIPLE, policy = ReferencePolicy.DYNAMIC)
+    public void bindOSGiFactoryConfig(final DemoConfig config)
+    {
+        if(configList == null){
+            configList = new ArrayList<>();
+        }
+        configList.add(config);
+    }
+
+    public void unbindOSGiFactoryConfig(final DemoConfig config)
+    {
+        configList.remove(config);
     }
 
     @Deactivate
@@ -36,7 +57,9 @@ public class DemoServiceImpl implements DemoService {
     @Override
     public String doSomething() {
         LOG.info("======DOING SOMETHING============");
-        return "Returning from the implementation DemoServiceImpl. Value of the OSGi Config some setting : "+someSetting;
+        return "Returning from the implementation DemoServiceImpl.\nValue(s) of the OSGi Config somesetting : \n\n\n\nHi";
     }
+
+     
 
 }
