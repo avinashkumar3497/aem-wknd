afb29affc6d2f0f7843e5987deb547c219e8da39
Avinash Kumar <avinashkumar3497@gmail.com>
2026-02-19 09:32:13 +0530

    multifield with composite true implemented

--- core/src/main/java/com/adobe/aem/guides/wknd/core/models/AuthorBooks1.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/AuthorBooks1.java
@@ -3,6 +3,7 @@ package com.adobe.aem.guides.wknd.core.models;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -11,12 +12,16 @@ import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.Default;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.Required;
+import org.apache.sling.models.annotations.injectorspecific.Self;
 
 @Model(
     adaptables = Resource.class
 )
 public class AuthorBooks1 {
     
+    @Self
+    Resource resource;
+
     @Inject
     @Default(values = "Default Value")
     @Required
@@ -27,9 +32,16 @@ public class AuthorBooks1 {
         return authorName;
     }
 
+    // @Inject
+    // List<String> booksName;
+
     public List<String> getBooksName(){
         List<String> l = new ArrayList<>();
-        Collections.addAll(l,"aaaa","bbbb","cccc","dddd");
+        Resource multifieldNodeResource = resource.getChild("booksName");
+        Iterator<Resource> itr  = multifieldNodeResource.getChildren().iterator();
+        while(itr.hasNext()){
+            l.add(itr.next().getValueMap().get("booksName",String.class));
+        }
         return l;
     }
 }
