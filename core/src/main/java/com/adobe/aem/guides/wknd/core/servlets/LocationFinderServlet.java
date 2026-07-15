6b537922b41878e9cf4dc7ae1e8096aaf374e040
Avinash Kumar <your.email@example.com>
2026-07-15 19:43:33 +0530

    Add configuration for LocationFinderServlet with API URL and Key

--- core/src/main/java/com/adobe/aem/guides/wknd/core/servlets/LocationFinderServlet.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/servlets/LocationFinderServlet.java
@@ -17,7 +17,14 @@ import org.apache.http.util.EntityUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Modified;
+import org.osgi.service.metatype.annotations.AttributeDefinition;
+import org.osgi.service.metatype.annotations.Designate;
+import org.osgi.service.metatype.annotations.ObjectClassDefinition;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Component(
     service = Servlet.class,
@@ -26,7 +33,12 @@ import org.osgi.service.component.annotations.Component;
         "sling.servlet.methods=GET"
     }
 )
+@Designate(ocd = LocationFinderServlet.Configuration.class)
 public class LocationFinderServlet extends SlingSafeMethodsServlet {
+
+    private static Logger LOG = LoggerFactory.getLogger(LocationFinderServlet.class);
+    private String api_Url="";
+    private String api_Key="";
 
     @Override
     protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) throws ServletException, IOException
@@ -37,14 +49,14 @@ public class LocationFinderServlet extends SlingSafeMethodsServlet {
 
         RequestConfig config = RequestConfig.custom().setConnectTimeout(2000).setSocketTimeout(5000).build();
 
-        String apiUrl = "https://geocoding-api.open-meteo.com/v1/search?name="  
+        String apiUrl = api_Url
                         + URLEncoder.encode(request.getParameter("query"), StandardCharsets.UTF_8.name())
                         + "&count=5&language=en&format=json";
 
         try (CloseableHttpClient httpClient = HttpClients.custom().setDefaultRequestConfig(config).build()) {
             
             HttpGet getRequest = new HttpGet(apiUrl);
-
+            LOG.info("value of apiUrl: {}",apiUrl);
             try (CloseableHttpResponse httpResponse = httpClient.execute(getRequest)) {
                 int status = httpResponse.getStatusLine().getStatusCode();
                 if(status == 200){
@@ -60,5 +72,30 @@ public class LocationFinderServlet extends SlingSafeMethodsServlet {
             response.setStatus(500);
             response.getWriter().write("{\"error\":\"" + e.getMessage() + "\"}");
         }
+    }
+
+    @Activate
+    @Modified
+    protected void activate(Configuration configuration){
+        LOG.info("API URL from config: {} | API Key from config: {}", configuration.apiUrlValue(), configuration.apiKeyValue());
+        api_Url=configuration.apiUrlValue();
+    }
+
+    @ObjectClassDefinition(
+        name = "config for Location Finder Servlet",
+        description = "configuration used for Location Finder Servlet"
+    )
+    public @interface Configuration{
+        
+        @AttributeDefinition(
+            name="API URL"
+        )
+        public String apiUrlValue() default "default_URL";
+
+        @AttributeDefinition(
+            name = "API Key"
+        )
+        public String apiKeyValue() default "default_key";
+
     }
 }
