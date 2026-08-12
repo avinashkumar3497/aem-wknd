0698b198493a2a7241a82dfbcd8fdbcbb5cc4529
Avinash Kumar <your.email@example.com>
2026-08-12 10:56:16 +0530

    feat(elasticsearch): [POC3][Task3] read pagePath + open service ResourceResolver

--- core/src/main/java/com/adobe/aem/guides/wknd/core/servlets/RocheEsCrawlServlet.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/servlets/RocheEsCrawlServlet.java
@@ -1,18 +1,25 @@
 package com.adobe.aem.guides.wknd.core.servlets;
 
 import java.io.IOException;
+import java.util.Map;
 
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.LoginException;
+import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.adobe.aem.guides.wknd.core.services.ElasticSearchConfigService;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 @Component(
     service = Servlet.class,
@@ -25,11 +32,13 @@ import com.adobe.aem.guides.wknd.core.services.ElasticSearchConfigService;
 )
 public class RocheEsCrawlServlet extends SlingSafeMethodsServlet{
     
+    private static final Logger LOG = LoggerFactory.getLogger(RocheEsCrawlServlet.class);
+
     @Reference
     private ElasticSearchConfigService configService;
     
     @Reference
-    private ResourceResolverFactory factory;
+    private ResourceResolverFactory resolverFactory;
 
     // commenting it until the helper is ready
     // @Reference
@@ -37,8 +46,35 @@ public class RocheEsCrawlServlet extends SlingSafeMethodsServlet{
 
     @Override
     protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) throws ServletException,IOException{
-        response.setContentType("text/plain");
+        final String pagePath = request.getParameter("pagePath");
+        JsonObject jsonObject = new JsonObject();
+        response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
-        response.getWriter().write("request recieved by RocheEsCrawlServlet");
+        
+        //fetching pagePath from request parameter
+        if(pagePath==null||pagePath.isBlank()){
+            jsonObject.addProperty("status", "error");
+            jsonObject.addProperty("message", "pagePath required");
+            response.setStatus(SlingHttpServletResponse.SC_BAD_REQUEST);
+        }
+        else{
+            try(ResourceResolver serviceResolver = resolverFactory.getServiceResourceResolver(Map.of(ResourceResolverFactory.SUBSERVICE,"es-crawl-user"))){
+                jsonObject.addProperty("status", "success");
+                jsonObject.addProperty("message", "pagePath recieved: "+pagePath);
+            }
+            catch(LoginException loginException){
+                jsonObject.addProperty("status", "error");
+                jsonObject.addProperty("message", "Login Exception occured with message: "+loginException.getMessage());
+                response.setStatus(SlingHttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            }
+            catch(Exception e){
+                LOG.error("Unexpected error in ES crawl servlet", e);
+                jsonObject.addProperty("status", "error");
+                jsonObject.addProperty("message", "Internal Server Error: Exception occured");
+                response.setStatus(SlingHttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            }
+        }
+        String jsonString = new Gson().toJson(jsonObject);
+        response.getWriter().write(jsonString);
     }
 }
