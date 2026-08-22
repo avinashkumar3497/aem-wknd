70a700f06fe1cf0094d93aaacc3f6bdf8923ab10
Avinash Kumar <your.email@example.com>
2026-08-22 08:20:57 +0530

    [Workflow POC1 - Task 6] add dynamic participant chooser for workflow initiator

---
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/workflows/WorkflowInitiatorParticipantChooser.java
@@ -0,0 +1,25 @@
+package com.adobe.aem.guides.wknd.core.workflows;
+
+import org.osgi.service.component.annotations.Component;
+
+import com.adobe.granite.workflow.WorkflowException;
+import com.adobe.granite.workflow.WorkflowSession;
+import com.adobe.granite.workflow.exec.ParticipantStepChooser;
+import com.adobe.granite.workflow.exec.WorkItem;
+import com.adobe.granite.workflow.metadata.MetaDataMap;
+
+@Component(
+    service = ParticipantStepChooser.class,
+    property = {
+        "chooser.label=Workflow Initiator"
+    }
+)
+public class WorkflowInitiatorParticipantChooser implements ParticipantStepChooser{
+
+    @Override
+    public String getParticipant(WorkItem workItem, WorkflowSession workflowSession, MetaDataMap args)
+            throws WorkflowException {
+        return workItem.getWorkflow().getInitiator();
+    }
+
+}
